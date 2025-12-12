1871ffc55782db3caa2fbe6d1fae818906fcd62c
colmehurze <colmehurze@gmail.com>
2025-12-12 19:38:17 +0530

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,17 @@
 # Downloading and setting up zsh and fastfetch
 pacman -Sy
-pacman -S --noconfirm zsh fastfetch
+pacman -S --noconfirm zsh fastfetch unzip
 # Downloading oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 # Downloading oh-my-posh
 curl -s https://ohmyposh.dev/install.sh | bash -s
 # Installing other packages
-pacman -S --noconfirm --needed base-devel git sudo nano vim unzip
+pacman -S --noconfirm --needed base-devel git sudo nano vim go
 # Setting up the new user
 ./user-setup.sh
 # Setting up yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
-chown -R user:user .
-sudo -u user makepkg -si
+useradd -m builder
+chown -R builder:builder .
+sudo -u builder makepkg -si
